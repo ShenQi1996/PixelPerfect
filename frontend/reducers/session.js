a1d70470b984538472288410e85a5292323dcd8a
ShenQi1996 <qisam1989@gmail.com>
2021-05-11 12:36:48 -0400

    test

--- frontend/reducers/session.js
+++ frontend/reducers/session.js
@@ -8,10 +8,10 @@ export default (state = _nullSession, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      console.log(action.type, state);
+      // console.log(action.type, state);
       return Object.assign({}, { currentUser: action.user });
     case LOGOUT_CURRENT_USER:
-      console.log(action.type, state);
+      // console.log(action.type, state);
       return _nullSession;
     default:
       return state;
